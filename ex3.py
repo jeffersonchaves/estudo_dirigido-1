ba5e79c2befa96329236908888850db19e50abb2
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-04-26 21:32:26 -0300

    Update ex3.py
    melhora no resultado do print

--- ex3.py
+++ ex3.py
@@ -3,5 +3,4 @@ hrs = int(input("Informe as horas "))
 min = int(input("Informe os minutos "))
 sec = int(input("Informe os segundos "))
 
-
-print( (dias*24*3600) + (hrs*3600) + (min*60) + sec , "segundos")
+print( "Total de", (dias*24*3600) + (hrs*3600) + (min*60) + sec , "segundos")
