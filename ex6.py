ffd1f18582fa7e653978c4bb061ce804b1b7096f
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-04-26 22:09:43 -0300

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,4 @@
+distancia = float(input("Informe a distancia em Km: "))
+velocidadeMedia = float(input("Informe a velocidade media e Km/h: "))
+
+print ( "Tempo de viagem sera de", distancia / velocidadeMedia , "horas")
