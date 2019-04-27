cfebdfb40a31647c24e6e359b6ecd1d1780e2b3f
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-04-26 21:30:27 -0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,7 @@
+dias = int(input("Informe os dias "))
+hrs = int(input("Informe as horas "))
+min = int(input("Informe os minutos "))
+sec = int(input("Informe os segundos "))
+
+
+print( (dias*24*3600) + (hrs*3600) + (min*60) + sec , "segundos")
