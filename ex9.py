cfb13e60d17ec27e4b4802e82661c284d9960653
alissonlegal <30471021+alissonlegal@users.noreply.github.com>
2019-04-26 22:24:44 -0300

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#cada cigarro -11 minutos, imprima a quantidade de dias perdidos para 
+#alguem que fume 10 cigarros por dia ha 3 anos
+
+perdMinuto=(11)
+qntCig=(10)
+anosFuma=(3*365)
+tempoPerdi=((perdMinuto)*(qntCig)*(anosFuma))
+
+minHora=(60)
+horaDia=(24)
+resuPerdi=((tempoPerdi)/(minHora)/(horaDia))
+print(resPerdi, "dias")	
+
+
